5bf0d88770854ec2600968bd506429dc90fa150f
carlogtt <carlogtt@amazon.co.uk>
2023-07-05 15:48:45 +0100

    Add string_tools feature: snake_case

--- my_utils_library/utils/string_tools.py
+++ my_utils_library/utils/string_tools.py
@@ -10,11 +10,9 @@ This module contains useful functions to work with strings.
 # Importing required libraries and modules for the application.
 
 # Standard Library Imports ---------------------------------------------------------------------------------------------
+import logging
 import random
 import string
-
-# Local Application Imports --------------------------------------------------------------------------------------------
-from ..logger import master_logger
 
 # END IMPORTS ----------------------------------------------------------------------------------------------------------
 
@@ -23,7 +21,7 @@ from ..logger import master_logger
 # __all__ = [...]
 
 # Setting up logger for current module
-my_app_logger = master_logger.get_child_logger(__name__)
+module_logger = logging.getLogger(__name__)
 
 
 def get_random_string(length: int) -> str:
@@ -31,5 +29,55 @@ def get_random_string(length: int) -> str:
     return ''.join(random.choice(secret) for _ in range(length))
 
 
+def snake_case(string_to_normalize: str) -> str:
+    """
+    Normalize the given string by converting uppercase letters to lowercase and replacing whitespaces with underscores.
+    The resulting string is stripped of leading and trailing underscores.
+
+    Args:
+        string_to_normalize:
+            The original string that needs to be normalized.
+
+    Returns:
+        The normalized string with all uppercase characters converted to lowercase and all whitespaces replaced by
+        underscores.
+    """
+    result = []
+
+    for idx, ch in enumerate(string_to_normalize):
+
+        # Replace all whitespaces with underscore
+        if ch in string.whitespace:
+            ch = "_"
+
+            # if the last character is already an underscore then skip it
+            if result and result[-1] == "_":
+                continue
+
+        # Check if an underscore is part of the string_to_normalize
+        elif ch in ["_", "-"]:
+            ch = "_"
+
+            # if the last character is already an underscore then skip it
+            if result and result[-1] == "_":
+                continue
+
+        # Convert uppercase characters to lowercase
+        elif ch in string.ascii_uppercase:
+            ch = ch.casefold()
+
+            # if the last character is already an underscore then skip it
+            if result and not result[-1] == "_":
+                result.append("_")
+
+        result.append(ch)
+
+    # Clean up possible leading and trailing underscores coming from the composition loop
+    result = "".join(result).strip("_")
+
+    return result
+
+
 if __name__ == '__main__':
-    pass
+    s = "  Hello __  this i7Is--My Name  "
+    print(snake_case(s))
