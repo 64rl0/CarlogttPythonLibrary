db4dc3c4118dd177ecf34a6dae803202e354d867
carlogtt <carlogtt@amazon.co.uk>
2023-07-06 09:20:27 +0100

    Update module description

--- my_utils_library/__init__.py
+++ my_utils_library/__init__.py
@@ -3,16 +3,18 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 """
-This module contains the package imports for the current package.
+my_utils_library is a collection of utility functions designed to simplify common tasks in Python.
 """
 
 # IMPORTS --------------------------------------------------------------------------------------------------------------
 # Importing required libraries and modules for the application.
 
 # Local Folder (Relative) Imports --------------------------------------------------------------------------------------
-# from .database import *
-# from .exceptions import *
-# from .logger import *
+
+# Imports for all the modules except for utils have been disables as the objective of this package is to be used
+# with specific submodule imports.
+# i.e. import my_utils_library.database to access the database utils.
+# i.e. import my_utils_library.logger to access the logger utils.
 from .utils import *
 
 # END IMPORTS ----------------------------------------------------------------------------------------------------------
