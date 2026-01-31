db85e3a0454530b9a96b9591584f83bcb90be3bb
carlogtt <carlogtt@amazon.com>
2026-01-31 12:58:41 +0000

    library updates

--- scripts/build_read_the_docs.sh
+++ scripts/build_read_the_docs.sh
@@ -4,8 +4,8 @@
 #  (      _ \     /  |     (   | (_ |    |      |
 # \___| _/  _\ _|_\ ____| \___/ \___|   _|     _|
 
-# scripts/deploy_pypi.sh
-# Created 1/24/25 - 6:46 PM UK Time (London) by carlogtt
+# scripts/build_read_the_docs.sh
+# Created 1/31/26 - 12:34 PM UK Time (London) by carlogtt
 # Copyright (c) Amazon.com Inc. All Rights Reserved.
 # AMAZON.COM CONFIDENTIAL
 
@@ -54,6 +54,7 @@ declare -r end_dim=$'\033[22m'
 declare -r end_italic_underline=$'\033[23m'
 declare -r end_invert=$'\033[27m'
 declare -r end_hidden=$'\033[28m'
+declare -r clear_line=$'\033[2K'
 
 # Emoji
 declare -r green_check_mark="\xE2\x9C\x85"
@@ -70,8 +71,8 @@ declare -r package="\xF0\x9F\x93\xA6"
 declare -r network_world="\xF0\x9F\x8C\x90"
 
 # Script Options
-set -o errexit  # Exit immediately if a command exits with a non-zero status
-set -o pipefail # Exit status of a pipeline is the status of the last cmd to exit with non-zero
+set -o errexit   # Exit immediately if a command exits with a non-zero status
+set -o pipefail  # Exit status of a pipeline is the status of the last cmd to exit with non-zero
 
 # Script Paths
 script_dir_abs="$(realpath -- "$(dirname -- "${BASH_SOURCE[0]}")")"
@@ -79,36 +80,23 @@ declare -r script_dir_abs
 project_root_dir_abs="$(realpath -- "${script_dir_abs}/..")"
 declare -r project_root_dir_abs
 
-# We accept this because of false gitleak failures
-icarus builder release || true
+if [[ -z "${CARLOGTT_SECRET_READ_THE_DOCS_API_KEY}" ]]; then
+    echo "${bold_red}[ERROR]${end} - CARLOGTT_SECRET_READ_THE_DOCS_API_KEY is not set"
+    exit 1
+fi
 
-echo -e "\n\n"
+# Read the Docs Project
+project_name="carlogttpythonlibrary"
+project_version="latest"
+url="https://app.readthedocs.org/api/v3/projects/${project_name}/versions/${project_version}/builds/"
 
-# Update version on file
-. "${script_dir_abs}/update_version.sh"
-echo -e "\n\n"
+response=$(curl -s -X POST -H "Authorization: Token ${CARLOGTT_SECRET_READ_THE_DOCS_API_KEY}" "${url}")
 
-# Commit changes
-pushd "${project_root_dir_abs}" >/dev/null
-git add pyproject.toml
-git commit -m "bump version to -> ${new_version}"
-git push
-popd >/dev/null
+echo -e "\n\n${bold_green}${hammer_and_wrench} Read the Docs${end}"
+echo -e "Follow the build progress at:"
+echo "${response}" | jq -r '.build.urls.build'
+echo -e "\n${bold_green}${green_check_mark} Build started successfully ${end}\n"
+echo
 
-# Clean environment BEFORE
-echo -e "\n\n${bold_green}${broom} Cleaning environment${end}"
-rm -rf "${project_root_dir_abs}/dist"
-echo -e "completed!"
 
-# Build and push from the icarus env
-icarus builder exec ". ${script_dir_abs}/build_and_push.sh"
-
-# Clean environment AFTER
-echo -e "\n\n${bold_green}${broom} Cleaning environment${end}"
-rm -rf "${project_root_dir_abs}/dist"
-echo -e "completed!"
-echo -e "\n\n"
-
-# Update Read the Docs
-"${script_dir_abs}/build_read_the_docs.sh"
 
